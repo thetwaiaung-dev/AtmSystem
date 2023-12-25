b9528921b50d3c8df6b8aee5254245b8b7eb7f43
Thet Wai Aung <twa453354@gmail.com>
2023-12-25 11:50:59 +0630

    [Complete] Add AtmCard , User models

---
+++ ATM/Models/AtmCardModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ATM.Models
+{
+    [Table("atm-card")]
+    public class AtmCardModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CardNo { get; set; }
+        public int Amount { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
