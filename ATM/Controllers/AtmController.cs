8b7e82c4dec86e9cfef52e57df593703176cb79f
Thet Wai Aung <twa453354@gmail.com>
2023-12-25 11:15:45 +0630

    [Complete] Atm UI Account Details page

---
+++ ATM/Controllers/AtmController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATM.Controllers
+{
+    public class AtmController : Controller
+    {
+        [ActionName("account-detail")]
+        public IActionResult AccountDetail()
+        {
+            return View("AccountDetail");
+        }
+    }
+}
